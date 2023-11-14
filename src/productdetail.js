a05836bfab6900934790f018031972ac7f5361ab
Rahultota <rahultota2001@gmail.com>
2023-11-14 08:37:15 +0530

    add product

--- src/productdetail.js
+++ src/productdetail.js
@@ -1,3 +1,15 @@
+import tp1 from './img/tp1.jpg'
+import tp2 from './img/tp2.jpg'
+import tp3 from './img/tp3.jpg'
+import tp4 from './img/tp4.jpg'
+import tp5 from './img/tp5.jpg'
+import tp6 from './img/tp6.png'
+import tp7 from './img/tp7.jpg'
+import tp8 from './img/tp8.jpg'
+import p9 from './img/p9.jpg'
+import p10 from './img/p10.jpg'
+import p11 from './img/p11.jpg'
+import p12 from './img/p12.jpg'
 const Productdetail =
 [
     {
@@ -5,84 +17,84 @@ const Productdetail =
         Title:"Galaxy Tab S6 Lite 10.4-inch Android Tablet 128GB.",
         Cat: 'Tablet',
         Price: '723',
-        Img: './img/tp1.jpg'
+        Img: tp1
     },
     {
         id:2,
         Title:"Tracker with IP67 Waterproof Pedometer Smart watch.",
         Cat: 'Smart Watch',
         Price: '168',
-        Img: './img/tp2.jpg'
+        Img: tp2
     },
     {
         id:3,
         Title:"Cancelling Headphones Wireless.",
         Cat: 'Headphone',
         Price: '49',
-        Img: './img/tp3.jpg'
+        Img:tp3
     },
     {
         id:4,
         Title:"Professional Camera 4K Digital Video Camera.",
         Cat: 'Camera',
         Price: '1049',
-        Img: './img/tp4.jpg'
+        Img: tp4
     },
     {
         id:5,
         Title:"Mini Portable PD 22.5W Fast Charging Powerbank.",
         Cat: 'Powerbank',
         Price: '49',
-        Img: './img/tp5.jpg'
+        Img: tp5
     },
     {
         id:6,
         Title:"CPU Cooler 2 Heat Pipes 12mm 4 Pin PWM RGB for Intel.",
         Cat: 'Electronics',
         Price: '156',
-        Img: './img/tp6.png'
+        Img: tp6
     },
     {
         id:7,
         Title:"Playstation 4 2TB Slim Gaming Console.",
         Cat: 'Gaming',
         Price: '2098',
-        Img: './img/tp7.jpg'
+        Img: tp7
     },
     {
         id:8,
         Title:"Mini Portable Mobile Phone Powerbank for iphone.",
         Cat: 'Electronics',
         Price: '386',
-        Img: './img/tp8.jpg'
+        Img: tp8
     },
     {
         id:9,
         Title:"Microsoft Surface Pro 8-13' Touchscreen.",
         Cat: 'Tablet',
         Price: '693',
-        Img: './img/p9.jpg'
+        Img: p9
     },
     {
         id:10,
         Title:"Playstation 4 2TB Slim Gaming Console.",
         Cat: 'Gaming',
         Price: '5036',
-        Img: './img/p10.jpg'
+        Img: p10
     },
     {
         id:11,
         Title:"Echo Show 5 (2nd Gen) Adjustable Stand | Charcoal",
         Cat: 'Electronics',
         Price: '198',
-        Img: './img/p11.jpg'
+        Img: p11
     },
     {
         id:12,
         Title:"Echo Dot smart speaker",
         Cat: 'Electronics',
         Price: '793',
-        Img: './img/p12.jpg'
+        Img: p12
     },
 ]
 export default Productdetail;
